201c34eb635429d8bf6bfb32a447bbc06e73fac0
Rocha Neto <seu-email@exemplo.com>
2026-09-07 12:32:55 -0300

    Permissao de execucao e instalacao do LibreOffice no build

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Sair em caso de erro
+# Sair imediatamente em caso de erro
 set -o errexit
 
-# Atualizar pacotes e instalar LibreOffice
+# Atualizar repositórios e instalar LibreOffice no container Linux
 apt-get update && apt-get install -y libreoffice
 
-# Instalar dependencias Python
+# Instalar as dependências do Python
 pip install -r requirements.txt
